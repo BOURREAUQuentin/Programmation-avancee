a8839adb3011edc9d54d656e0b34336389003e30
Quentin_BOURREAU <quentin.bourreau@etu.univ-orleans.fr>
2024-09-04 21:18:48 +0200

    début de liaison avec un modele de bd (classe Task)

--- GestionProduits/LesProduits/models.py
+++ GestionProduits/LesProduits/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(auto_now=True)
+    schedule_date = models.DateTimeField(auto_now=True)
+    closed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name + ": " + self.description
