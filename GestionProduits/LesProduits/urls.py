0143c50b33510459be28aefa177b8d9cfcede590
Compte Temporaire INFO 3 <temporaire-info3@or-iut-i102-f13.iut45.univ-orleans.fr>
2024-09-05 08:39:32 +0200

    fix: correction urls

--- GestionProduits/LesProduits/urls.py
+++ GestionProduits/LesProduits/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home),
-    path('home/<name>', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('home/<name>', views.hello, name='hello'),
 ]
--- GestionProduits/LesProduits/views.py
+++ GestionProduits/LesProduits/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def home(request, name = ""):
-    return HttpResponse("Bonjour depuis Django : " + name)
+def home(request):
+    return HttpResponse("Bienvenue sur l'accueil")
+
+def hello(request, name = ""):
+    return HttpResponse("<h1>Bonjour " + name + " content de vous revoir ici !</h1>")
