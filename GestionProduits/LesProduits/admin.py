1274a574105d20a5d8f68543981372284b2a4cb7
BOURREAUQuentin <bourreauquentin@gmail.com>
2024-09-12 09:50:28 +0200

    feat: ajout derniers fonctionnalités

--- GestionProduits/LesProduits/admin.py
+++ GestionProduits/LesProduits/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Product, ProductItem, ProductAttribute, ProductAttributeValue
 
+def set_product_online(modeladmin, request, queryset):
+    queryset.update(status=1)
+set_product_online.short_description = "Mettre en ligne"
+
+def set_product_offline(modeladmin, request, queryset):
+    queryset.update(status=0)
+set_product_offline.short_description = "Mettre hors ligne"
+
 class ProductItemAdmin(admin.TabularInline):
     model = ProductItem
     filter_vertical = ("attributes",)
@@ -24,12 +32,17 @@ class ProductFilter(admin.SimpleListFilter):
 class ProductAdmin(admin.ModelAdmin):
     model = Product
     inlines = [ProductItemAdmin,]
-    list_display = ('id', 'name', 'date_creation', 'status')
-    list_editable = ['name']
+    list_display = ('id', 'name', 'date_creation', 'status', "price_ht", "price_ttc","tax")
+    list_editable = ['name', "price_ht", "price_ttc"]
     radio_fields = {"status": admin.VERTICAL}
     search_fields = ('name',)
     list_filter = (ProductFilter,)
     date_hierarchy = 'date_creation'
+    actions = [set_product_online, set_product_offline]
+    
+    def tax(self, instance):
+        return (instance.price_ttc / instance.price_ht)-1
+    tax.short_description = "Taxes (%)"
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductItem)
